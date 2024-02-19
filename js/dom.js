eb2777f05f73dc1b1282ff77bce6f347c5f7dab7
Tariqul-22 <tariqul.abf@gmail.com>
2024-02-19 21:51:17 +0600

    finish

--- js/dom.js
+++ js/dom.js
@@ -1,17 +1,47 @@
-// const allSeats = document.getElementsByClassName('seat');
+const seats = document.getElementsByClassName('seat');
+const seatHolder = document.getElementById('seatHolder');
+
+
+
+function addTicket(event){
+    console.log(event.target.innerText);
+    const innerDiv = document.createElement('div')
+
+    const p1 =document.createElement('p')
+    const p2 =document.createElement('p')
+    const p3 =document.createElement('p')
+
+    const nodeP1 = document.createTextNode(event.target.innerText)
+    const nodeP2 = document.createTextNode('Economy')
+    const nodeP3 = document.createTextNode('550')
+
+    p1.appendChild(nodeP1)
+    p2.appendChild(nodeP2)
+    p3.appendChild(nodeP3)
+
+    innerDiv.appendChild(p1)
+    innerDiv.appendChild(p2)
+    innerDiv.appendChild(p3)
+
+    innerDiv.classList.add('flex')
+    innerDiv.classList.add('justify-between')
+    innerDiv.classList.add('items-center')
+
+    seatHolder.appendChild(innerDiv)
+}
+
+for(const seat of seats){
+    // console.log(seat);
+    seat.addEventListener('click', addTicket)
+}
+
+
+
 let count = 40;
 let i=0;
 let j=0;
 let l=0;
 let k=0;
-// for(const seat of allSeats){
-//     seat.addEventListener('click',function () {
-//         console.log('seat conferm');
-        
-
-        
-//     });
-// }
 
 function setInnerText (id, value){
     document.getElementById(id).innerText = value ;
