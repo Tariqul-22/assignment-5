b6c03ce0d7bf55c371e60105fbc739a358b1641d
Tariqul-22 <tariqul.abf@gmail.com>
2024-02-19 19:29:04 +0600

    calculate total price

--- js/dom.js
+++ js/dom.js
@@ -1,19 +1,38 @@
-const allSeats = document.getElementsByClassName('seat');
+// const allSeats = document.getElementsByClassName('seat');
 let count = 40;
-for(const seat of allSeats){
-    seat.addEventListener('click',function () {
-        console.log('seat conferm');
-        count = count - 1;
-
-        setInnerText ('available-seat', count);
+let i=0;
+let j=0;
+let l=0;
+let k=0;
+// for(const seat of allSeats){
+//     seat.addEventListener('click',function () {
+//         console.log('seat conferm');
         
-    });
-}
+
+        
+//     });
+// }
 
 function setInnerText (id, value){
     document.getElementById(id).innerText = value ;
 }
 
 function setBackground (id){
-    
+    if (k < 4) {
+    count = count - 1;
+    i=i+1;
+    j=j+550;
+    l=l+550;
+    setInnerText ('available-seat', count);
+    setInnerText ('total-seat', i);
+    setInnerText ('total-price', j);
+    setInnerText ('grand-total', l);
+
+    let element = document.getElementById(id);
+    element.classList.remove("bg-[#F7F8F8]");
+    element.classList.add("bg-[#1DD100]");
+
+   
+    }
+    k=k+1;
 }
